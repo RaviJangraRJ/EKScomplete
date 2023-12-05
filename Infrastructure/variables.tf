87d5d3b98fd907456353031dd0e27869ba4959ec
RaviJangraRJ <100367393+RaviJangraRJ@users.noreply.github.com>
2023-12-05 20:58:50 +0530

    added variables

---
+++ Infrastructure/variables.tf
@@ -0,0 +1,24 @@
+variable "vpc_cidr" {
+    type = string
+    description = "vpc cidr"
+}
+
+variable "subnet_cidr1" {
+  type = string
+  description = "subnet cidr"
+}
+
+variable "subnet_cidr2" {
+  type = string
+  description = "subnet cidr"
+}
+
+variable "subnet_cidr3" {
+  type = string
+  description = "subnet cidr"
+}
+
+variable "subnet_cidr4" {
+  type = string
+  description = "subnet cidr"
+}
